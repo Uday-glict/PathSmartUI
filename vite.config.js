3e34ded930a16667dd4245d2274fd928fa3ef466
Uday-glict <84915848+Uday-glict@users.noreply.github.com>
2025-11-11 14:27:50 +0530

    versel file check check

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: 'dist', // Vercel expects this directory by default
+    outDir: 'dist',
   },
-  base: '/', // ensures correct routing for SPAs on Vercel
-  server: {
-    port: 5173,
-    open: true,
-  },
+  base: '/', // important for SPA routing
 })
